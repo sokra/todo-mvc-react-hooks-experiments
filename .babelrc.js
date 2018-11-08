8b6589296120a07a3e40059b8a900fb784de7857
Jason Miller <developit@google.com>
2018-11-08 14:49:10 -0500

    Add proptypes removal plugin

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,7 @@ module.exports = {
 	presets: ["@babel/preset-env", "@babel/preset-react"],
 	plugins: [
 		"@babel/plugin-proposal-class-properties",
+		"babel-plugin-transform-react-remove-prop-types",
 		"rawact"
 	]
 };
